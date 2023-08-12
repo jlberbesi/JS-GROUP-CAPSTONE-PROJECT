fff788ea1bf9cf12122b8bb738f5f34da63802bd
CarlosZ96 <c4r1oszambrano@gmail.com>
2023-08-12 17:32:13 -0500

    Update
    fix

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@
 import './styles.css';
 import Like from './like.png';
 import showComments from './modules/popup.js';
-import { LikeBtn, UpdateLikes } from './Api.js';
+import { LikeBtn } from './Api.js';
 
 const apiUrl = 'https://api.tvmaze.com/shows';
 const ShowsID = [];
