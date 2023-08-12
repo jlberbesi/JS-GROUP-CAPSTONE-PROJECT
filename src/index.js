93807770ca86ee77a069b32b43a5b06c38755251
CarlosZ96 <c4r1oszambrano@gmail.com>
2023-08-12 15:46:31 -0500

    Update:
    Creating getLikes, AddLikes,  LikeBtn and   UpdateLikes fuctions.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 /* eslint-disable linebreak-style */
-// eslint-disable-next-line import/extensions, import/no-unresolved
+/* eslint-disable camelcase */
 import './styles.css';
+import Like from './like.png';
 import showComments from './modules/popup.js';
+import { LikeBtn, UpdateLikes } from './Api.js';
 
 const apiUrl = 'https://api.tvmaze.com/shows';
-
+const ShowsID = [];
 async function fetchItems() {
   try {
     const response = await fetch(apiUrl);
@@ -22,12 +24,22 @@ function renderItems(items) {
   items.forEach((item) => {
     const itemElement = document.createElement('div');
     itemElement.className = 'item';
-    itemElement.innerHTML = `<img class="poppup__img" src="${item.image.medium}"><h2>${item.name}</h2>`;
+    itemElement.innerHTML = `<img class="poppup__img" src="${item.image.medium}"><h2>${item.name}</h2>
+    <div class="item-buttons">
+    <button class="item-like-btn" data-item-id="${item.id}" aria-label="Like"><img class="like" src="${Like}" alt=""></button>
+  </div>`;
+    ShowsID.push(item.id);
+
+    const likesCount = document.createElement('span');
+    likesCount.className = 'likes_count';
+    likesCount.innerText = '0';
+    itemElement.appendChild(likesCount);
+
+    LikeBtn(itemElement.querySelector('.item-like-btn'), likesCount);
 
     const commentsButton = document.createElement('button');
     commentsButton.innerText = 'Show Comments';
     commentsButton.addEventListener('click', () => showComments(item));
-
     itemElement.appendChild(commentsButton);
     itemsContainer.appendChild(itemElement);
   });
